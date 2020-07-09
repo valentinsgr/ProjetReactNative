33ae2fce2c24042fa5b4bee01a120280740ffe48
valentinsgr <valentin77350@gmail.com>
2020-07-09 15:19:49 +0200

    add style.js

---
+++ Style.js
@@ -0,0 +1,29 @@
+
+export default {
+    color: '#A2273C',
+    container: {
+        margin:20
+    },
+    title:{
+        fontSize:22,
+        marginBottom:20
+    },
+    input: {
+        height: 40, 
+        borderColor: 'gray',
+        borderWidth: 1, 
+        paddingHorizontal:10, 
+        marginBottom: 20
+    },
+    button: {
+        backgroundColor: '#A2273C',
+        color: '#FFFFFF'
+    },
+    header: {
+        backgroundColor: '#A2273C',
+    },
+    headerTitle:{
+        color:'#FFF'
+    }
+
+}
